cccbab4d827f9f1a8d339fedf835449cf7d49eaf
jhonatan lopez <55928962+jhonatanlopez13@users.noreply.github.com>
2020-06-21 09:32:24 -0500

    modificaciones visuales de cliente solo falta oferta y login y registro

--- includes/menulateralusuario.php
+++ includes/menulateralusuario.php
@@ -1,9 +1,9 @@
 <?php
-require_once "model/conexion.php";
-//require_once "../../model/validarSesion.php";
-require_once "model/consulta.php";
-//require_once "../../controller/admin/verPerfil.php";
-//require_once "../../model/seguridad.php";
+require_once "../../model/conexion.php";
+require_once "../../model/validarSesion.php";
+require_once "../../model/consulta.php";
+require_once "../../controller/admin/verPerfil.php";
+require_once "../../model/seguridad.php";
 ?>
 <div class="left-sidebar-pro">
             <nav id="sidebar" class="">
@@ -17,29 +17,13 @@ require_once "model/consulta.php";
                         <li class="active">
                             <a class="has-arrow" href="index.php">
                                 <i class="fa big-icon fa-home icon-wrap"></i>
-                                <span class="mini-click-non">Atento Empleos</span>
+                                <span class="mini-click-non">Administrador</span>
 							</a>
                             <ul class="submenu-angle" aria-expanded="true">
                             <!-- //crud usuario -->
-                                <li><a title="registro" href="registrarUsuarioStandar.php"><i class="fa fa-edit" aria-hidden="true"></i> <span class="mini-sub-pro">Registrate</span></a></li>
-                                <!-- <li><a title="ver-usuarios" href="listaUsuarios.php"><i class="fa fa-eye nav-icon" aria-hidden="true"></i> <span class="mini-sub-pro">Lista Usuarios</span></a></li> -->
-                                <!-- crud centro -->
-                                <!-- <li><a title="registro" href="registrarCentros.php"><i class="fa fa-edit" aria-hidden="true"></i> <span class="mini-sub-pro">Registrar Centros</span></a></li>
-                                <li><a title="ver-centros" href="listaCentros.php"><i class="fa fa-eye nav-icon" aria-hidden="true"></i> <span class="mini-sub-pro">Lista Centros</span></a></li> -->
-                                <!-- crud vehiculo -->
-                                <!-- <li><a title="registro" href="registrarVehiculos.php"><i class="fa fa-edit" aria-hidden="true"></i> <span class="mini-sub-pro">Registrar Vehiculo</span></a></li>
-                                <li><a title="ver-usuarios" href="listaVehiculos.php"><i class="fa fa-eye nav-icon" aria-hidden="true"></i> <span class="mini-sub-pro">Lista Vehiculos</span></a></li> -->
-                                <!-- crud producto -->
-                                <!-- <li><a title="registro" href="registrarOferta.php"><i class="fa fa-edit" aria-hidden="true"></i> <span class="mini-sub-pro">Registrar oferta</span></a></li>
-                                <li><a title="ver-usuarios" href="listaOfertas.php"><i class="fa fa-eye nav-icon" aria-hidden="true"></i> <span class="mini-sub-pro">Lista Ofertas</span></a></li> -->
-                                <!-- crud solicitud -->
-                                <!-- <li><a title="registro" href="registrarSolicitud.php"><i class="fa fa-edit" aria-hidden="true"></i> <span class="mini-sub-pro">Registrar Solicitud</span></a></li> -->
-                                <!-- <li><a title="ver-usuarios" href="listaSolicitud.php"><i class="fa fa-eye nav-icon" aria-hidden="true"></i> <span class="mini-sub-pro">Lista Solicitudes</span></a></li> -->
-                                <!-- crud rutas -->
-                                <!-- <li><a title="registro" href="registrarRutas.php"><i class="fa fa-edit" aria-hidden="true"></i> <span class="mini-sub-pro">Registrar Rutas</span></a></li>
-                                <li><a title="ver-usuarios" href="listaRutas.php"><i class="fa fa-eye nav-icon" aria-hidden="true"></i> <span class="mini-sub-pro">Lista Rutas</span></a></li> -->
+                                <li><a title="registro" href="editarUsuario.php"><i class="fa fa-edit" aria-hidden="true"></i> <span class="mini-sub-pro">Editar Usuario</span></a></li>
+                                <li><a title="ver-usuarios" href="listaProductos.php"><i class="fa fa-eye nav-icon" aria-hidden="true"></i> <span class="mini-sub-pro">Lista Ofertas</span></a></li>
                             </ul>
-                        <li><a title="Landing Page" href="" aria-expanded="false"><i class="fa fa-bookmark icon-wrap sub-icon-mg" aria-hidden="true"></i> <span class="mini-click-non">Landing Page</span></a></li>
                     </ul>
                 </nav>
             </div>
