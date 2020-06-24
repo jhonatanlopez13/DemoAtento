00a793b49adfcb71592d9dac1de77f41e69b9585
jhonatan lopez <55928962+jhonatanlopez13@users.noreply.github.com>
2020-06-24 14:25:27 -0500

    login

--- login.php
+++ login.php
@@ -1,95 +1,36 @@
 <!doctype html>
-<html class="no-js" lang="en">
+<html class="no-js" lang="es">
 
 <head>
-    <meta charset="utf-8">
-    <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <title>Login | jeweler - Material Admin Template</title>
-    <meta name="description" content="">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="css/style2.css">
-    <!-- favicon
-		============================================ -->
-    <link rel="shortcut icon" type="image/x-icon" href="img/favicon.ico">
-    <!-- Google Fonts
-		============================================ -->
-    <link href="https://fonts.googleapis.com/css?family=Play:400,700" rel="stylesheet">
-    <!-- Bootstrap CSS
-		============================================ -->
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-    <!-- Bootstrap CSS
-		============================================ -->
-    <link rel="stylesheet" href="css/font-awesome.min.css">
-    <!-- owl.carousel CSS
-		============================================ -->
-    <link rel="stylesheet" href="css/owl.carousel.css">
-    <link rel="stylesheet" href="css/owl.theme.css">
-    <link rel="stylesheet" href="css/owl.transitions.css">
-    <!-- animate CSS
-		============================================ -->
-    <link rel="stylesheet" href="css/animate.css">
-    <!-- normalize CSS
-		============================================ -->
-    <link rel="stylesheet" href="css/normalize.css">
-    <!-- main CSS
-		============================================ -->
-    <link rel="stylesheet" href="css/main.css">
-    <!-- morrisjs CSS
-		============================================ -->
-    <link rel="stylesheet" href="css/morrisjs/morris.css">
-    <!-- mCustomScrollbar CSS
-		============================================ -->
-    <link rel="stylesheet" href="css/scrollbar/jquery.mCustomScrollbar.min.css">
-    <!-- metisMenu CSS
-		============================================ -->
-    <link rel="stylesheet" href="css/metisMenu/metisMenu.min.css">
-    <link rel="stylesheet" href="css/metisMenu/metisMenu-vertical.css">
-    <!-- calendar CSS
-		============================================ -->
-    <link rel="stylesheet" href="css/calendar/fullcalendar.min.css">
-    <link rel="stylesheet" href="css/calendar/fullcalendar.print.min.css">
-    <!-- forms CSS
-		============================================ -->
-    <link rel="stylesheet" href="css/form/all-type-forms.css">
-    <!-- style CSS
-		============================================ -->
-    <link rel="stylesheet" href="style.css">
-    <!-- responsive CSS
-		============================================ -->
-    <link rel="stylesheet" href="css/responsive.css">
-    <!-- modernizr JS
-		============================================ -->
-    <script src="js/vendor/modernizr-2.8.3.min.js"></script>
+<?php require 'views/head.php'; ?>
 </head>
 <body>
-  
+
    <!-- Cabecera y menu -->
 <header id="header-home">
   <div class="hero">
     <div class="container-fluid">
       <div class="container-log">
         <div class="row-back">
-              <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
-                  <div class="col-md-1">
-                      <a href="index.html">
-                          <img width="300" height="200" src="img/logo/logoatento.png" alt="logo de la pagina">
+              <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                  <div class="col-md-4">
+                      <a href="<?php echo constant('URL'); ?>">
+                          <img src="<?php echo constant('URL'); ?>public/img/SILTO.png" alt="logo de la pagina">
                       </a>
                   </div>
-              </div>  
-              <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                  <div class="back-link back-backend">
-                      <a href="index.php" class="btn btn-primary">INICIO</a>
+                  <div class="col-md-4"></div>
+                  <div class="col-md-4">
+                  <a href="<?php echo constant('URL'); ?>" class="btn btn-danger">INICIO</a>
                   </div>
+              </div>
+              <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                  <div class="back-link back-backend"></div>
               </div>
         </div>
         <div class="row-log">
             <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12"></div>
             <div class="col-md-4 col-md-4 col-sm-4 col-xs-6">
-                <div class="text-center m-b-md custom-login">
-                    <h1>INICIA SESION</h1>
-                    <!-- <p>Este es tu mejor sitio</p> -->
-                </div>
-                <div class="hpanel">
+            <div class="hpanel">
                     <div class="panel-body">
                         <form action="controller/admin/iniciarSesion.php"  method="POST">
                             <div class="form-group">
@@ -116,59 +57,13 @@
             <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12"></div>
         </div>
       </div>
-      
-    </>
+    </div>
   </div>
 </header>
-
-
-
-
-    <!-- jquery
-		============================================ -->
-    <script src="js/vendor/jquery-1.11.3.min.js"></script>
-    <!-- bootstrap JS
-		============================================ -->
-    <script src="js/bootstrap.min.js"></script>
-    <!-- wow JS
-		============================================ -->
-    <script src="js/wow.min.js"></script>
-    <!-- price-slider JS
-		============================================ -->
-    <script src="js/jquery-price-slider.js"></script>
-    <!-- meanmenu JS
-		============================================ -->
-    <script src="js/jquery.meanmenu.js"></script>
-    <!-- owl.carousel JS
-		============================================ -->
-    <script src="js/owl.carousel.min.js"></script>
-    <!-- sticky JS
-		============================================ -->
-    <script src="js/jquery.sticky.js"></script>
-    <!-- scrollUp JS
-		============================================ -->
-    <script src="js/jquery.scrollUp.min.js"></script>
-    <!-- mCustomScrollbar JS
-		============================================ -->
-    <script src="js/scrollbar/jquery.mCustomScrollbar.concat.min.js"></script>
-    <script src="js/scrollbar/mCustomScrollbar-active.js"></script>
-    <!-- metisMenu JS
-		============================================ -->
-    <script src="js/metisMenu/metisMenu.min.js"></script>
-    <script src="js/metisMenu/metisMenu-active.js"></script>
-    <!-- tab JS
-		============================================ -->
-    <script src="js/tab.js"></script>
-    <!-- icheck JS
-		============================================ -->
-    <script src="js/icheck/icheck.min.js"></script>
-    <script src="js/icheck/icheck-active.js"></script>
-    <!-- plugins JS
-		============================================ -->
-    <script src="js/plugins.js"></script>
-    <!-- main JS
-		============================================ -->
-    <script src="js/main.js"></script>
+<?php require 'views/footer.php'; ?>
+<script>
+  txtusu.focus();
+</script>
 </body>
 
 </html>
